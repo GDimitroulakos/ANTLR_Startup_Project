77792c3407dfe0f3fd22d067c32af543cd9563c4
Grigoris Dimitroulakos <dhmhgre@gmail.com>
2019-10-16 12:39:22 +0300

    Step 3: Ignite parser and print the syntax tree in textual form

--- Program.cs
+++ Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 
 namespace ANTLR_Startup_Project
 {
     class Program
     {
         static void Main(string[] args) {
+            StreamReader astream = new StreamReader("test.txt");
+
+            AntlrInputStream antlrStream = new AntlrInputStream(astream);
+
+            firstLexer lexer = new firstLexer(antlrStream);
+
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+            firstParser parser = new firstParser(tokens);
+
+            IParseTree tree =parser.compileUnit();
+
+            Console.WriteLine(tree.ToStringTree());
+
         }
     }
 }
